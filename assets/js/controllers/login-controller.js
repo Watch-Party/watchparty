46843c5fe4a71df61a6f205e7ab0f1555ac001b1
David <dgrayboff@gmail.com>
2016-07-18 15:25:58 -0400

    learn more functionality

--- assets/js/controllers/login-controller.js
+++ assets/js/controllers/login-controller.js
@@ -3,16 +3,24 @@ watchParty.controller('loginCtrl', function($scope){
 
   $scope.login = true;
   $scope.signup = true;
+  $scope.about = true;
 
   $scope.showLogin = function(){
     $scope.login = false;
     $scope.signup = true;
+    $scope.about = true;
   }
   $scope.showSignup = function(){
     $scope.login = true;
     $scope.signup = false;
+    $scope.about = true;
   }
 
+  $scope.aboutShow = function(){
+    $scope.about = false;
+    $scope.login = true;
+    $scope.signup = true;
+  }
 
 
 
