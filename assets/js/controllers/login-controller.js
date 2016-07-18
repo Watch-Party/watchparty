a050c639fe26056c08b9e3c6aaed261948306844
David <dgrayboff@gmail.com>
2016-07-18 16:19:14 -0400

    added input models for login and signup in controller - can console.log values

--- assets/js/controllers/login-controller.js
+++ assets/js/controllers/login-controller.js
@@ -1,26 +1,52 @@
 watchParty.controller('loginCtrl', function($scope){
 
-
+  // On load of page: //
   $scope.login = true;
   $scope.signup = true;
   $scope.about = true;
 
+  // Login input fields //
   $scope.showLogin = function(){
     $scope.login = false;
     $scope.signup = true;
     $scope.about = true;
   }
+
+  // Signup input fields //
   $scope.showSignup = function(){
     $scope.login = true;
     $scope.signup = false;
     $scope.about = true;
   }
 
+  // Learn more text //
   $scope.aboutShow = function(){
     $scope.about = false;
     $scope.login = true;
     $scope.signup = true;
   }
+
+  // On submit of GO, log username and password inputs to console //
+  $scope.submitLogin = function() {
+    var loginInfo = {
+      'username': $scope.username,
+      'password': $scope.password
+    }
+    console.log(loginInfo);
+  }
+
+  $scope.saveNewUser = function() {
+    var newUserInfo = {
+      'firstName': $scope.firstName.toLowerCase(),
+      'lastName': $scope.lastName.toLowerCase(),
+      'email': $scope.email.toLowerCase(),
+      'username': $scope.newUsername,
+      'password': $scope.newPassword, // authentication here -- if both password values match, send data -- else, outline text box in red //
+      'password2': $scope.newPassword2
+    }
+    console.log(newUserInfo);
+  }
+
 
 
 
