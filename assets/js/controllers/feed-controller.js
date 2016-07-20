59c0cbff2f4485805012ea6fc37b383df56f39af
David <dgrayboff@gmail.com>
2016-07-20 10:50:38 -0400

    can display a post in feed and clear input form

--- assets/js/controllers/feed-controller.js
+++ assets/js/controllers/feed-controller.js
@@ -1,15 +1,24 @@
-watchParty.controller('postCtrl', function($scope, $http){
-
+watchParty.controller('postCtrl', function($scope, $http, $compile){
+  $scope.allPosts= [];
   $scope.submitPost = function() {
-    $scope.post = {
-        'content': $scope.postContent
-    }
-    console.log($scope.post);
+    $scope.post =  {
+        'content': $scope.postContent,
+      };
+        $scope.allPosts.push($scope.post);
+        console.log($scope.allPosts);
+        console.log($scope.post);
+        $scope.postContent = '';
+
+  }
+  $scope.reset = function(){
+
+  }
+
 
     // $http.post('https://json-party.herokuapp.com/users/sign_in.json', $scope.post).then(function(post){
     //   console.log(post);
     // })
-  }
+
 
 
 
