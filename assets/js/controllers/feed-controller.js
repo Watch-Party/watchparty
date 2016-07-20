bc96764516d5f090f545b47e808e4e35a0c477c8
David <dgrayboff@gmail.com>
2016-07-20 11:10:03 -0400

    created filter for reverse post display

--- assets/js/controllers/feed-controller.js
+++ assets/js/controllers/feed-controller.js
@@ -4,11 +4,11 @@ watchParty.controller('postCtrl', function($scope, $http, $compile){
     $scope.post =  {
         'content': $scope.postContent,
       };
+        // $filter('orderBy')($scope.post, indexOf, reverse)
         $scope.allPosts.push($scope.post);
         console.log($scope.allPosts);
         console.log($scope.post);
         $scope.postContent = '';
-
   }
   $scope.reset = function(){
 
@@ -23,4 +23,11 @@ watchParty.controller('postCtrl', function($scope, $http, $compile){
 
 
 
+});
+
+// inspiration from: https://codedump.io/share/Eunu1YNUTbAO/1/angular-ng-repeat-in-reverse //
+watchParty.filter('missyElliot', function() {
+  return function(posts) {
+  return posts.slice().reverse();
+};
 });
